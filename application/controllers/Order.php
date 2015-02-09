04b9f644046a032b5ee01d7ca14e457e84831a8d
w-chiang <williampwc@gmail.com>
2015-02-09 15:46:27 -0800

    bug fixes after testing line 111 in order controller was redirecting on true rather than false

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -108,7 +108,7 @@ class Order extends Application {
 
     // proceed with checkout
     function commit($order_num) {
-        if($this->orders->validate($order_num))
+        if(!$this->orders->validate($order_num))
             redirect('/order/display_menu/' . $order_num);
         $record = $this->orders->get($order_num);
         $record->date = date(DATE_ATOM);
